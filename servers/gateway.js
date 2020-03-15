b58b72b7d32bb95f7edcdfa2e48cb58047eb3837
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-14 20:47:43 -0700

    Replaced port # for HW1

--- servers/gateway.js
+++ servers/gateway.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
 const app = express();
-const port = process.env.PORT || 80;
+const port = 5000;
 
 const apiProxy = httpProxy.createProxyServer();
 
