47c88a2198c455bfb1c5185da59e54a858bc655b
BERNIE <bernie@BERNIEs-MacBook-Pro.local>
2020-03-20 23:01:10 -0700

    removed some files

---
+++ client/src/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className='back-page'>
+                <div className="container1">
+
+
+                    <h1 className='welcome'>Welcome Home</h1>
+                    <div className="container">
+                        <img
+                            src="https://archive.org/download/under-construction/under-construction.jpg"
+                            alt="logo"
+                        />
+                    </div>
+                </div>
+
+
+            </div>
+
+        );
+    }
+}
+
+export default Landing;
---
+++ client/src/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const NavigationBar = () => {
+    return (
+      <div className="navigation-bar">
+        <Link to="/">
+          <h1>CSS 667 HW2</h1>
+        </Link>
+        <Link to="/bdiaz666">
+          <div className="about">bdiaz666</div>
+        </Link>
+        <Link to="/ctam4">
+                <div className="about">ctam4</div>
+        </Link>
+        <Link to="/jason1819">
+                <div className="about">jason1819</div>
+        </Link>
+      </div>
+    );
+}
+
+export default NavigationBar
