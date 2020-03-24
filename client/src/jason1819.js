195c6066f7e36711d5196fa5bb778b5bd42210d8
jason1819 <jxie3@mail.sfsu.edu>
2020-03-24 13:39:45 -0700

    call api from hw1 and update the page UI

--- client/src/jason1819.js
+++ client/src/jason1819.js
@@ -1,14 +1,55 @@
-import React from "react";
-import './jason1819.css';
+ import React from "react";
+ import './jason1819.css';
 
-const teamName = ({city}) => {
-  return (
-    <div className='page'>
-       <h2>To find a nba team name, please enter a city</h2>
-      <input value={city} id="input-field" placeholder=""></input>
-      <button>search</button>
-    </div>
-  );
+const getTeamName = (e) => {
+  e.preventDefault();
+  const city = document.getElementById('input-field');
+  const result = document.getElementById('output-field');
+  result.innerHTML = 'Loading....';
+  const url= "/api/jason1819/team_name?city=" + city.value;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+         console.log(data);
+      if(data.status === 'OK'){
+         result.innerHTML = data.response;
+      }
+      else{
+         result.innerHTML = 'Error: ' + data.response;
+      }
+    })
+    .catch((err) => result.innerHTML = err);
 };
 
-export default teamName;
+const jasonPage = () =>{
+  return(
+    <section>
+      <div className="header">
+           <h1>Welcome to my page</h1>
+      </div>
+      <div className="center">
+          <img width="30%" src="https://imgur.com/aLKnUn5.jpg" alt="nba team logos"/>
+      </div>
+      <form onSubmit={getTeamName}>
+      <div className="center">
+        <h4>*To find a NBA team name, please enter a city</h4>
+      </div>
+        <div className="center">
+          <input
+            id="input-field"
+            type="text"
+            required
+            placeholder=" "
+          />
+        </div>
+           <div className="button">
+             <button>submit</button>
+           </div>
+       </form>
+      <div id="output-field" className="output">
+    </div>
+    </section>
+  );
+}
+
+export default jasonPage;
