d5e39e873e0331ff5f365b69ef1edd6a68bbaddf
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-25 23:20:26 -0700

    Fix docker-compose not connecting to HW1

--- servers/gateway.js
+++ servers/gateway.js
@@ -3,7 +3,7 @@ const httpProxy = require('express-http-proxy');
 const app = express();
 const port = 5000;
 
-app.use("/api", httpProxy("http://0.0.0.0"));
+app.use("/api", httpProxy("http://" + (process.env.API_HOST || "0.0.0.0")));
 app.use("/", httpProxy("http://0.0.0.0:4000"));
 
 app.listen(port, () => console.log(`Gateway on port ${port}!`))
