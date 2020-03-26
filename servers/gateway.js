d6b1c7939e0322d8553ebfe19bab591bf7226074
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-03-25 23:12:37 -0700

    Replaced http-proxy to express-http-proxy for cleaner code

--- servers/gateway.js
+++ servers/gateway.js
@@ -1,32 +1,9 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
+const httpProxy = require('express-http-proxy');
 const app = express();
 const port = 5000;
 
-const apiProxy = httpProxy.createProxyServer();
-
-apiProxy.on('error', (err, req, res) => {
-  console.log(err)
-  res.status(500).send('Proxy Error');
-});
-
-app.all("/api*", (req, res) => {
-  // service1
-  req.url = req.url.replace('/api', '');
-  req.originalUrl = req.originalUrl.replace('/api', '');
-  if (req.path) {
-    req.path = req.path.replace('/api', '');
-  }
-  apiProxy.web(req, res, {
-    target: 'http://0.0.0.0',
-  });
-});
-
-app.all("*", (req, res) => {
-  // front end server / react
-  apiProxy.web(req, res, {
-    target: 'http://0.0.0.0:4000',
-  });
-});
+app.use("/api", httpProxy("http://0.0.0.0"));
+app.use("/", httpProxy("http://0.0.0.0:4000"));
 
 app.listen(port, () => console.log(`Gateway on port ${port}!`))
